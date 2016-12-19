122709338c03120f0f49736afce3d0d25014753c
晚黎 <ibanya@126.com>
2016-12-19 13:39:36 +0800

    路由更改

--- routes/admin/media.php
+++ routes/admin/media.php
@@ -2,5 +2,5 @@
 $router->group(['prefix' => 'media'],function ($router)
 {
 	$router->get('/','MediaController@index')->name('media.list');
-	$router->get('iwanli/image/{img}','MediaController@destroy')->name('media.destroy');
+	$router->get('iwanli/'.config('admin.global.imagePath').'{img}','MediaController@destroy')->name('media.destroy');
 });
